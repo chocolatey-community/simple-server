2d959126b7a582d8a8614f1b429b93a00403a732
Rob Reynolds <ferventcoder@gmail.com>
2018-01-03 15:41:58 -0600

    (GH-10) use WebActivator Start for routes

---
+++ src/SimpleChocolateyServer/App_Start/NuGetODataConfig.cs
@@ -0,0 +1,30 @@
+using System.Net.Http;
+using System.Web.Http;
+using System.Web.Http.Routing;
+using NuGet.Server;
+using NuGet.Server.V2;
+
+[assembly: WebActivatorEx.PreApplicationStartMethod(typeof(SimpleChocolateyServer.App_Start.NuGetODataConfig), "Start")]
+
+namespace SimpleChocolateyServer.App_Start 
+{
+    public static class NuGetODataConfig 
+	{
+        public static void Start() 
+		{
+            ServiceResolver.SetServiceResolver(new DefaultServiceResolver());
+
+            var config = GlobalConfiguration.Configuration;
+
+            NuGetV2WebApiEnabler.UseNuGetV2WebApiFeed(config, "ChocolateyDefault", "chocolatey", "PackagesOData");
+
+            config.Routes.MapHttpRoute(
+                name: "NuGetDefault_ClearCache",
+                routeTemplate: "chocolatey/clear-cache",
+                defaults: new { controller = "PackagesOData", action = "ClearCache" },
+                constraints: new { httpMethod = new HttpMethodConstraint(HttpMethod.Get) }
+            );
+
+        }
+    }
+}
